bc5ea036b7e8b8b00ac8d9c15837a605dfa23a38
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2024-12-10 17:59:32 +0800

    Highlight Enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,18 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -50,5 +58,57 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 	{
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	/*
+	* 从光标开始的轨迹。有几种情况：
+	* 1.LastActor is null && ThisActor is null
+	*	- 什么都不做
+	* 2.LastActor is null && ThisActor is valid
+	*	- 突出显示(Highlight) ThisActor
+	* 3.LastActor is valid && ThisActor is null
+	*	- 取消突出显示(UnHighlight) LastActor
+	* 4.LastActor 和 ThisActor 都是 valid, 但是LastActor != ThisActor
+	*	- 取消突出显示 LastActor, 突出显示 ThisActor
+	* 5. LastActor 和 ThisActor 都是 valid, 并且LastActor == ThisActor
+	*	- 什么都不做
+	*/
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			// Case 2.
+			ThisActor->HighlightActor();
+		}
+		//Case 1.
+	}
+	else //LastActor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			//Case 3.
+			LastActor->UnHighlightActor();
+		}
+		else //ThisActor is valid
+		{
+			if (LastActor != ThisActor)
+			{
+				//Case 4.
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			//Case 5.
+		}
 	}
 }
