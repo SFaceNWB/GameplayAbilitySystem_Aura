e84329ee896052e49a52808e35e39cc1fd2997ca
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2024-12-10 19:53:39 +0800

    Constructing the ASC and AS

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -4,15 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "AbilitysystemInterface.h"
 #include "AuraPlayerState.generated.h"
+
+class UAbilitySystemComponent;
+class UAttributeSet;
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraPlayerState : public APlayerState
+class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 public:
 	AAuraPlayerState();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
+	
+	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
+protected:
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
