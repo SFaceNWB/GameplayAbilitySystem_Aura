527067c900e430356c3b4a79e45866100d07db51
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2025-02-17 14:41:08 +0800

    Player Level and Combat Interface

---
+++ Source/Aura/Private/Interaction/CombatInterface.cpp
@@ -0,0 +1,11 @@
+// SFaceNWB Game
+
+
+#include "Interaction/CombatInterface.h"
+
+// Add default functionality here for any ICombatInterface functions that are not pure virtual.
+
+int32 ICombatInterface::GetPlayerLevel()
+{
+	return 0;
+}
