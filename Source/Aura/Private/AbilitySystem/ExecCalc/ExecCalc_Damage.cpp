7739145e17a910c7aa5a6ce3feb2be8b606f2be0
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2025-03-20 10:30:15 +0800

    Mapping Damage Types to Resistances

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -67,9 +67,9 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	// Get Damage Set By Caller Mognitude
 	float Damage = 0.0f;
-	for (FGameplayTag DamageTypeTag : FAuraGameplayTags::Get().DamageTypes)
+	for (auto& Pair : FAuraGameplayTags::Get().DamageTypesToResistances)
 	{
-		const float DamageTypeValue = Spec.GetSetByCallerMagnitude(DamageTypeTag);
+		const float DamageTypeValue = Spec.GetSetByCallerMagnitude(Pair.Key);
 		Damage += DamageTypeValue;
 	}
 
