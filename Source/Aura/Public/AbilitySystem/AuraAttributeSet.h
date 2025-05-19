96905832f37b89dbbd56fcd4cc9fdd70191d8187
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2025-05-19 13:57:07 +0800

    Sending XP Events

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -246,4 +246,5 @@ private:
 
 	void SetEffectProperties(const struct FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
 	void ShowFloatingText(const FEffectProperties& Props, float Damage, bool bBlockedHit, bool bCriticalHit) const;
+	void SendXPEvent(const FEffectProperties& Props);
 };
