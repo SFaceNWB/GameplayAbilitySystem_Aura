548859c9399a8ac36a1b4de4c9001944c63af14d
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2025-06-30 14:52:01 +0800

    Spawn Volums

---
+++ Source/Aura/Private/Actor/AuraEnmeySpawnPoint.cpp
@@ -0,0 +1,18 @@
+// SFaceNWB Game
+
+
+#include "Actor/AuraEnmeySpawnPoint.h"
+
+#include "Character/AuraEnemy.h"
+
+void AAuraEnmeySpawnPoint::SpawnEnemy()
+{
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	AAuraEnemy* Enemy = GetWorld()->SpawnActorDeferred<AAuraEnemy>(EnemyClass, GetActorTransform());
+	Enemy->SetLevel(EnemyLevel);
+	Enemy->SetCharacterClass(CharacterClass);
+	Enemy->FinishSpawning(GetActorTransform());
+	Enemy->SpawnDefaultController();
+}
